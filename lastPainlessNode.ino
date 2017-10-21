9c02a2a0ad2404b4d18d816d4d35e75923d5c97e
Fabiana Ferreira Fonseca <fabiana.ferreira@poli.ufrj.br>
2017-10-21 16:26:54 -0200

    Last one

---
+++ lastPainlessNode.ino
@@ -0,0 +1,42 @@
+#include "painlessMesh.h"
+
+#define   MESH_PREFIX     "MeshNode"
+#define   MESH_PASSWORD   "MeshNode"
+#define   MESH_PORT       80
+
+painlessMesh  mesh;
+
+void receivedCallback(uint32_t from, String &msg) {
+  String reply = "Received";
+  Serial.printf("startHere: Received from %u msg=%s\n", from, msg.c_str());
+  mesh.sendSingle(from, reply);
+}
+
+void newConnectionCallback(uint32_t nodeId) {
+    Serial.printf("--> startHere: New Connection, nodeId = %u\n", nodeId);
+}
+
+void changedConnectionCallback() {
+    Serial.printf("Changed connections %s\n",mesh.subConnectionJson().c_str());
+}
+
+void nodeTimeAdjustedCallback(int32_t offset) {
+    Serial.printf("Adjusted time %u. Offset = %d\n", mesh.getNodeTime(),offset);
+}
+
+void setup() {
+  Serial.begin(115200);
+//mesh.setDebugMsgTypes( ERROR | MESH_STATUS | CONNECTION | SYNC | COMMUNICATION | GENERAL | MSG_TYPES | REMOTE ); // all types on
+  mesh.setDebugMsgTypes( ERROR | STARTUP );  // set before init() so that you can see startup messages
+
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, MESH_PORT, STA_AP, AUTH_WPA2_PSK, 1, PHY_MODE_11G, 82, 0, 1);
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+}
+
+void loop() {
+  mesh.update();
+}
+
